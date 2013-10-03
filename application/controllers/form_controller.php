b21bad950d6027c91faaa99ba7073f0538150151
Greg <gkodikara@gmail.com>
2013-10-04 00:25:13 +1000

    New form test

---
+++ application/controllers/form_controller.php
@@ -0,0 +1,40 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Form_controller extends CI_Controller {
+
+	function __construct() {
+		parent::__construct();
+	}
+
+	function index() {
+		$this->load->view('form');
+	}
+
+	function set_user_data() {
+		$form_data = $this->input->post("form_data");
+		
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+		curl_setopt($ch, CURLOPT_URL, "http://gkodikara:adm1n123!@#@http://54.252.155.211:5984/lead_engine");
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($form_data));
+		curl_setopt($ch, CURLOPT_POST, 1);
+		$tuData = curl_exec($ch); 
+		curl_close($ch);
+	}
+
+	function get_user_data($user_id = null) {
+
+		if (!isset($user_id)) {
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+			curl_setopt($ch, CURLOPT_URL, "http://gkodikara:gregory55jB@217.199.160.116:5585/facebook_app_users/_design/fb_users/_view/has_email/");
+			curl_setopt($ch, CURLOPT_HTTPGET, 1);
+			$data = curl_exec($ch); 
+			curl_close($ch);
+
+			echo json_encode($data);	
+		}
+
+	}
+
+
+}
---
+++ application/views/form.php
@@ -0,0 +1,397 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta http-equiv="content-language" content="en" />
+
+<link href="http://apps.1stsocial.com.au/themes/board_styling/rss_board.css" rel="stylesheet" type="text/css" />
+
+    <style type="text/css">
+        @charset "UTF-8";
+
+
+body {
+width:810px;
+overflow:hidden;
+margin:10px; padding:0; border:0;
+    background-color: #ffffff;
+    background-image: url(appbackground.jpg);
+    background-repeat: repeat-x;
+	background-attachment: fixed;
+	font-family: "lucida grande", tahoma, verdana, arial, sans-serif;
+
+}
+
+a img {
+	border:none;
+}
+
+a {
+	color:#59707B;
+	text-decoration:none;
+	font-weight:700;
+}
+a:hover {
+	text-decoration:none;
+}
+a:active, a:visited, a:focus
+{
+	outline: none;
+	border: 0;
+}
+
+a:focus
+{
+	-moz-outline-style: none;
+	outline: 0;
+}
+
+a:focus, *:focus {
+    noFocusLine: expression(this.onFocus=this.blur());
+}
+
+#dynamic-content a:hover{
+	text-decoration: underline;
+}
+
+sub {
+display: none;
+}
+
+.rssFeed {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 12px;
+	
+}
+.rssFeed a {
+	color: #3B5998;
+	font-size: 18px;
+	text-decoration: none;
+}
+.rssFeed a:hover {
+	color: #3B5998;
+	text-decoration: underline;
+}
+
+.rssHeader { padding: 0.2em 0; }
+
+.rssBody { border-bottom: 2px solid #C1C1C1;
+border-top: 1px solid #C1C1C1; }
+.rssBody ul { list-style: none; }
+.rssBody ul, .rssRow, .rssRow h4, .rssRow p {
+	margin: 0;
+	padding: 0;
+}
+
+.rssRow { padding: 0.8em;
+border-left: 1px solid #C1C1C1;
+border-right: 1px solid #C1C1C1;
+border-top: 1px solid #C1C1C1; }
+.rssRow h4 { font-size: 8px; }
+.date {
+	font-size: ;
+	color: #666;
+	margin: 0.2em 0 0.4em 0;
+}
+
+li:nth-child(odd){ background-color: #ffffff; }
+li:nth-child(even){ background-color: #EDEFF4; }
+
+#contentlist li:nth-child(odd){ background-color: transparent; }
+#contentlist li:nth-child(even){ background-color: transparent; }
+
+.rssRow:active{
+background-color:#EFEFEF;
+}
+.rssRow:hover{
+background-color:#EFEFEF;
+}
+
+.wrap :hover{
+background-color:#DEE3EC;
+}
+
+.rssRow .rssMedia {
+	padding: 0.5em;
+	font-size: 1em;
+}
+
+.submitCV {
+border-left: 1px solid #C1C1C1;
+border-right: 1px solid #C1C1C1;
+border-top: 2px solid #94A3C4;
+border-bottom: 2px solid #C1C1C1;
+margin-bottom: 10px;
+background-color: #EDEFF4;
+padding: 10px;
+font-family: Arial, Helvetica, sans-serif;
+	font-size: 12px;
+	text-decoration: none;
+	color: #000;
+	text-align: center;
+ }
+ 
+ .dropdown {
+border-left: 1px solid #C1C1C1;
+border-right: 1px solid #C1C1C1;
+border-top: 2px solid #94A3C4;
+border-bottom: 2px solid #C1C1C1;
+background-color: #EDEFF4;
+font-family: Arial, Helvetica, sans-serif;
+	font-size: 12px;
+	text-decoration: none;
+	color: #000;
+	text-align: center;
+ }
+ .submitCV-outer {
+
+	text-decoration: none;
+
+ }
+
+ .source {
+font-family: Arial, Helvetica, sans-serif;
+	color: #3B5998;
+	font-size: 14px;
+	text-decoration: none;
+	padding:10px;
+	font-weight:bolder;
+	text-align: center;
+}
+ 
+ div.submitCV:active{
+background-color:#EFEFEF;
+}
+div.submitCV:hover{
+background-color:#EFEFEF;
+}
+
+ .submitCV-SUBMIT {
+font-family: Arial, Helvetica, sans-serif;
+	color: #3B5998;
+	font-size: 20px;
+	text-decoration: none;
+	padding:10px;
+	font-weight:bolder;
+	text-align: center;
+		text-decoration: none;
+}
+
+ 
+/*
+#5c75a9 background color
+#8a9cc2 top border
+#29447e border
+#1a356e border bottom
+#4f6aa3 hover background color
+*/
+ 
+.bottom {
+position:relative;
+margin-top: -40px;
+border:none;
+font-family: Arial, Helvetica, sans-serif;
+	font-size: 8px;
+	float: right;
+	width: 104px;
+
+}
+.facebook {
+
+	font-size: ;
+	color: #666;
+	margin: 0.2em 0 0.4em 0;
+	
+
+}
+
+.qrcode {
+
+float: right;
+
+}
+
+
+ 
+.button_outside_border_blue{
+width:100px;
+border:solid #29447e 1px;
+border-bottom:solid #1a356e 1px;
+cursor:pointer;
+float: right;
+}
+ 
+.button_inside_border_blue{
+padding:6px 0 6px 0;
+background-color:#5c75a9;
+border-top:solid #8a9cc2 1px;
+text-align:center;
+color:#ffffff;
+font-size:12px;
+font-weight:bolder;
+}
+ 
+div.button_inside_border_blue:active{
+background-color:#4f6aa3;
+}
+div.button_inside_border_blue:hover{
+background-color:#4f6aa3;
+}
+
+.jobtitle {
+font-family: Arial, Helvetica, sans-serif;
+	color: #3B5998;
+	font-size: 20px;
+	text-decoration: none;
+	font-weight:bolder;
+}
+
+.desc {
+font-family: Arial, Helvetica, sans-serif;
+	color: #000000;
+	font-size: 12px;
+
+}
+
+.desc a:link {
+font-family: Arial, Helvetica, sans-serif;
+
+	font-size: 12px;
+
+}
+
+#contentbox {
+padding: 10px; 
+font-family: Arial, Helvetica, sans-serif;
+	font-size: 12px;
+	font-weight: normal;
+	color: #333;
+	line-height: 1.5em;
+background-color: rgba(255,255,255,0.6);
+}
+
+h1 {
+    clear: both;
+    font-family: Arial,Helvetica,sans-serif;
+    font-size: 22px;
+    line-height: 100%;
+    margin-bottom: 25px;
+}    
+</style>
+
+
+
+
+
+<!--<script type="text/javascript" src="http://platform.linkedin.com/in.js">
+  api_key: lbxpg24kn490
+  authorize: true
+</script>-->
+
+</head>
+<body>
+<!--<script type="in/Login">
+"Hello, " + firstName + lastName 
+</script>-->
+
+
+<form id="form1" name="form1" method="post" action="">
+<table width="790px" border="0" cellspacing="0" cellpadding="5">
+  <tr>
+    <td valign="top" width="520"><table width="790" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td width="150"><label for="first_name">First Name</label></td>
+        <td width="630">
+          <input type="text" name="first_name" id="first_name" /></td>
+      </tr>
+      <tr>
+        <td><label for="last_name">Last Name</label></td>
+        <td>
+          <input type="text" name="last_name" id="last_name" /></td>
+      </tr>
+      <tr>
+        <td><label for="phone_number">Phone Number</label></td>
+        <td>
+          <input type="text" name="phone_number" id="phone_number" /></td>
+      </tr>
+      <tr>
+        <td><label for="email_address">Email Address</label></td>
+        <td>
+          <input type="text" name="email_address" id="email_address" /></td>
+      </tr>
+      <tr>
+        <td><label for="investment_properties">Number of Investment Properties currently owned</label></td>
+        <td> <input type="text" name="investment_properties" id="investment_properties" /></td>
+      </tr>
+      <tr>
+        <td>By clicking here you agree to be contacted in regards to this property:</td>
+        <td><input type="checkbox" name="agree" id="agree" />
+          <label for="agree">I Agree</label></td>
+      </tr>
+      <tr>
+        <td>&nbsp;</td>
+        <td>
+          <div onclick="crossDomainPost('form1')" class="button_outside_border_blue"><div class="button_inside_border_blue">Submit</div></div>
+
+        </td>
+      </tr>
+    </table>
+      
+  
+        
+    </td>
+  </tr>
+</table>
+</form>
+<div id="fb-root"></div>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+<script type="text/javascript">
+function crossDomainPost(oFormId) {
+    //Iterate through the form input elements
+    var oFormData = {};
+    $.each($("#" + oFormId + " input"), function(iIndex, oObj){
+      sInputName = $(oObj).attr("name");
+      sInputValue = $(oObj).val();
+      oFormData[sInputName] = sInputValue;
+    });
+    
+    // Add the iframe with a unique name
+    $(".xss-iframe").remove();
+    var iframe = document.createElement("iframe");
+    iframe.className = "xss-iframe";
+    var uniqueString = "throa.com";
+    document.body.appendChild(iframe);
+    iframe.style.display = "none";
+    iframe.contentWindow.name = uniqueString;
+
+    // construct a form with hidden inputs, targeting the iframe
+    jQuery(".xss-form").remove();
+    var form = document.createElement("form");
+    form.className = "xss-form"
+    form.target = uniqueString;
+    form.action = "http://tool.throa.com/throa/index.php/tracking/log_click/";
+    form.method = "POST";
+
+    $.each(oPostData, function(iIndex, oObj){
+            // repeat for each parameter
+            var input = document.createElement("input");
+            input.type = "hidden";
+            input.name = iIndex;
+            input.value = oObj;
+            form.appendChild(input);
+    });
+
+
+    document.body.appendChild(form);
+    form.submit();
+
+    //Clean up
+    jQuery(".xss-iframe").remove();
+    jQuery(".xss-form").remove();
+
+
+}
+</script>
+</body>
+</html>
